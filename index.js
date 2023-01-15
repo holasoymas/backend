d215b8274b00ebb51bbdf9feff6a6864a4364ec2
holasoymas <71905956331mahesh@gmail.com>
2023-01-15 22:49:47 +0545

    phonebook added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const data = require('./data')
 const cors = require('cors')
+const phonedata = require('./phonedata')
 
 app.use(express.static('build'))
 app.use(express.json())
@@ -10,6 +11,7 @@ app.use(cors())
 const PORT = process.env.PORT || 8000
 
 let notes = data.notes;
+let persons = phonedata.persons
 
 // app.get("/", (req, res) => {
 //   res.send('<h1>This is backend. nothing is in here use "/api/notes" to see data </h1>')
@@ -17,6 +19,21 @@ let notes = data.notes;
 
 app.get("/api/notes", (req, res) => {
   res.json(notes)
+})
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons)
+})
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const find = persons.find(person => person.id === id);
+
+  if (!find) {
+    res.status(404).send('<h2>Data out of limit</h2>')
+  } else {
+    res.json(find)
+  }
 })
 
 app.get("/api/notes/:id", (req, res) => {
